a13377ae18830766c1163e1b662ff410bfc51227
Hadeer Fawzy <hadeer.fawzy@accorpa.com>
2018-04-16 23:45:54 +0200

    check if content changes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -103,11 +103,26 @@ $(function() {
     }); 
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-
+    describe('New Feed Selection', function() {
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        
+        var firstContent;
 
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                firstContent = $('.feed').html();
+            });
+            done();
+        });
+
+        it('check if content changes', function(done) {
+          var secondContent = $('.feed').html();  
+          expect(secondContent).not.toEqual(firstContent);
+          done();
+        }); 
+    }); 
 
 }());
